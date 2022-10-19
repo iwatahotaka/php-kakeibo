f5a7921edca5c89fe70e4fee5cde94cd738639ce
iwatahotaka <115777656+iwatahotaka@users.noreply.github.com>
2022-10-19 15:42:49 +0800

    画面で入力された値を取得

--- create.php
+++ create.php
@@ -1,1 +1,15 @@
 <?php
+
+//処理の流れ
+//1,画面で入力された値を取得する
+//2,DBと連携
+//3,SQL文を作成して画面で入寮された値をrecordsテーブルに追加
+//4,index.phpに画面遷移する
+
+//1,画面で入力された値を取得
+$date = $_POST['date'];
+$title = $_POST['title'];
+$amount = $_POST['amount'];
+$type = $_POST['type'];
+
+?>
