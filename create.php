8e218c40515f383672c1639e745025ac415f63b8
iwatahotaka <115777656+iwatahotaka@users.noreply.github.com>
2022-10-20 13:51:22 +0800

    デーら保存/index.phpに遷移

--- create.php
+++ create.php
@@ -16,6 +16,23 @@ $title = $_POST['title'];
 $amount = $_POST['amount'];
 $type = $_POST['type'];
 
+// SQL文を作成
+$sql = "INSERT INTO records(title, type, amount, date, created_at, updated_at) VALUES(:title, :type, :amount, :date, now(), now())";
 
+//作成したSQLを実行できるように準備
+$stmt = $pdo->prepare($sql);
+
+//値の設定、何を保存するのかを設定  bind=結びつける、PARAM=パラメーター、外部から投入されたデータ
+$stmt->bindParam(':title', $title, PDO::PARAM_STR);
+$stmt->bindParam(':type', $type, PDO::PARAM_INT);
+$stmt->bindParam(':amount', $amount, PDO::PARAM_INT);
+$stmt->bindParam(':date', $date, PDO::PARAM_STR);
+
+//SQLを実行
+$stmt->execute();
+
+//index.htmlに移動
+header('Location: ./index.php');
+exit;
 
 ?>
