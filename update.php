77bfb48934a6eff87d8f1444e899d5fe14f8ab1a
iwatahotaka <115777656+iwatahotaka@users.noreply.github.com>
2022-10-21 13:52:55 +0800

    update実装

---
+++ update.php
@@ -0,0 +1,27 @@
+<?php
+include_once('./dbconnect.php');
+
+//画面で入力された値を取得
+$date = $_POST['date'];
+$title = $_POST['title'];
+$amount = $_POST['amount'];
+$type = $_POST['type'];
+$id = $_POST['id'];
+//var_dump($_POST);
+
+//SQL文を作成
+$sql = "UPDATE records SET title = :title, type = :type, amount = :amount, date = :date, updated_at = now() WHERE id = :id";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->bindParam(':title', $title, PDO::PARAM_STR);
+$stmt->bindParam(':type', $type, PDO::PARAM_INT);
+$stmt->bindParam(':amount', $amount, PDO::PARAM_INT);
+$stmt->bindParam(':date', $date, PDO::PARAM_STR);
+$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+$stmt->execute();
+
+header('Location: ./index.php');
+exit;
+?>
